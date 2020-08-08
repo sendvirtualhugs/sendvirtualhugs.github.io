005b4c41ba70a3c835b3e3a44903988d7cd815ac
bcko <bcko@pop-os.localdomain>
2020-08-07 19:56:21 -0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# sendvirtualhugs.github.io
---
+++ _next/static/1SmfngWXY1k54YvblvRzV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-b3607fb9dd8a1e9a90f2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a98d98d178927e175cb7.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
