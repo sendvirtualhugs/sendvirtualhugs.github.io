0df4ba9a9658de5440b99c276933d22e104bc80b
bcko <bcko@pop-os.localdomain>
2020-08-09 22:10:08 -0700

    Updates

---
+++ _next/static/aUK5x5OHWsuZUOFyJK7M5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-45c5fd0759073c45bf3b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ca6a368b57d4262d533d.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
